7f36e56942be3893bb0bbf878e227e7e80bdcde1
Wesley Lo <wesleylo1995@gmail.com>
2022-01-19 14:49:40 -0600

    Step 3 complete

--- index.js
+++ index.js
@@ -9,8 +9,17 @@ function addMovie(event) {
     movieTitle.textContent = inputField.value;
     movie.appendChild(movieTitle)
 
+    const deleteBtn = document.createElement('button');
+    deleteBtn.textContent = 'x';
+    deleteBtn.addEventListener('click',deleteMovie);
+    movie.appendChild(deleteBtn);
+
     document.querySelector('ul').appendChild(movie)
     inputField.value = ''
+}
+
+function deleteMovie(event) {
+    event.target.parentNode.remove();
 }
 
 let form = document.querySelector('form');
