cd6c3c8cb2be57579471306a0bc22be82ee6d474
Wesley Lo <wesleylo1995@gmail.com>
2022-01-19 14:36:17 -0600

    Summary 2 complete

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 // console.log('hello world')
 
 function addMovie(event) {
-    
+    event.preventDefault()
+    let inputField = document.querySelector('input')
+
+    let movie = document.createElement('li')
+    let movieTitle = document.createElement('span')
+    movieTitle.textContent = inputField.value;
+    movie.appendChild(movieTitle)
+
+    document.querySelector('ul').appendChild(movie)
+    inputField.value = ''
 }
+
+let form = document.querySelector('form');
+
+form.addEventListener('submit',addMovie)
